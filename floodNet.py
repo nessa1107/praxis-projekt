27a6975e85de4ab239d3dde2ac84dfa1a9de19c2
vommer <vanessa_maria.ommer@smail.th-koeln.de>
2024-05-31 09:37:58 +0200

    floodNet added

---
+++ floodNet.py
@@ -0,0 +1,14 @@
+from torch.utils.data import DataLoader
+from dataLoaderFloodNet import DataLoaderFloodNet
+
+
+batch_size = 16
+num_classes = 10
+
+train_dataset = DataLoaderFloodNet(folder_path=r'FloodNet\train')
+val_dataset = DataLoaderFloodNet(folder_path=r'FloodNet\val')
+test_dataset = DataLoaderFloodNet(folder_path=r'FloodNet\test')
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
