5a49b1cac858a80e85db08091c33cc350b755f4b
nessa1107 <vanessa_maria.ommer@smail.th-koeln.de>
2024-06-11 11:41:57 +0200

    base structure ready

---
+++ classNames.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ClassNames(Enum):
+    background = 0
+    building_flooded = 1
+    building_non_flooded = 2
+    road_flooded = 3
+    road_non_flooded = 4
+    water = 5
+    tree = 6
+    vehicle = 7
+    pool = 8
+    grass = 9
--- testing.py
+++ testing.py
@@ -3,7 +3,9 @@ from tqdm import tqdm
 import uNet
 from floodNet import test_loader, num_classes
 from visualisation import visualize_prediction
-from iouCalculator import compute_mean_iou
+from iouCalculator import compute_mean_iou, compute_iou_per_class
+import numpy as np
+from classNames import ClassNames
 
 print("Modules imported successfully.")
 
@@ -20,6 +22,7 @@ print("Model set to evaluation mode.")
 
 num_examples = len(test_loader)
 iou = 0
+iou_per_class_accumulator = [0] * num_classes
 for i, (images, labels) in enumerate(tqdm(test_loader)):
     images = images.to(device)
     labels = labels.to(device)
@@ -29,11 +32,28 @@ for i, (images, labels) in enumerate(tqdm(test_loader)):
     predictions = torch.argmax(outputs, dim=1)
 
     iou += compute_mean_iou(labels, predictions)
+    iou_per_class = compute_iou_per_class(predictions, labels, num_classes)
 
-    if (i + 1) % 5 == 0 or (i + 1) == num_examples-1:
+    for cls in range(num_classes):
+        iou_per_class_accumulator[cls] += iou_per_class[cls]
+
+    if (i + 1) % 50 == 0 or (i + 1) == num_examples-1:
         image = images[0]
         prediction = predictions[0].cpu().numpy()
         label = labels[0].cpu().squeeze().numpy()
+
+        unique, counts = np.unique(prediction, return_counts=True)
+        total_pixels = prediction.size
+        class_percentages = {cls: (count / total_pixels) * 100 for cls, count in zip(unique, counts)}
+
+        for cls, percentage in class_percentages.items():
+            class_name = ClassNames(cls).name.replace('_', ' ')
+            print(f'The Picture contains {percentage:.2f}% of {class_name}')
+
         visualize_prediction(image, prediction, label)
 
+mean_iou_per_class = [iou / num_examples for iou in iou_per_class_accumulator]
+
+for cls in range(num_classes):
+    print(f'Class {cls} IoU: {mean_iou_per_class[cls]}')
 print(f'\rTest Mean IoU: {iou/num_examples:.4f}')
